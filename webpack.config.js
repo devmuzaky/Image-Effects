27be40fa214f7c3963cf2b0d8e89c6214907a233
moe_zaky <devmuzaky@gmail.com>
2023-04-30 21:41:07 +0300

    finishing configuration webpack and node

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './public/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-    }
+        filename: 'index.js',
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './public/index.html',
+
+        }),
+    ],
+
 }
