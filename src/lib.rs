d9cb34dd2abebe682f9366be4c3fc3112c91119e
moe_zaky <devmuzaky@gmail.com>
2023-05-01 12:38:31 +0300

    fix the output issue

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,31 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
-use base64::{ encode, decode };
+use base64::{encode, decode};
 use image::load_from_memory;
 use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
+pub fn grayscale(encoded_file: &str) -> String {
+    log(&"Grayscale called".into());
 
-pub fn grayscale(encoded_file: &str)-> String {
-    pub fn grayscale(encoded_file: &str) -> String {
-        log(&"Grayscale called".into());
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
 
-        let base64_to_vector = decode(encoded_file).unwrap();
-        log(&"Image decoded".into());
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded".into());
 
-        let mut img = load_from_memory(&base64_to_vector).unwrap();
-        log(&"Image loaded".into());
+    img = img.grayscale();
+    log(&"Grayscale effect applied".into());
 
-        img = img.grayscale();
-        log(&"Grayscale effect applied".into());
+    let mut buffer: std::io::Cursor<Vec<u8>> = std::io::Cursor::new(Vec::new());
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"New image written".into());
 
-        let mut buffer = vec![];
-        img.write_to(&mut buffer, Png).unwrap();
-        log(&"New image written".into());
+    let encoded_img = encode(&buffer.into_inner());
+    let data_url = format!(
+        "data:image/png;base64,{}",
+        encoded_img
+    );
 
-        let encoded_img = encode(&buffer);
-        let data_url = format!(
-            "data:image/png;base64,{}",
-            encoded_img
-        );
-
-        data_url
-
-
+    data_url
 }
